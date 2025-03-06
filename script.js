3b118c74ad069fb350dca041d4cb956f48dc80f2
WardahKhurram127 <149463720+WardahKhurram127@users.noreply.github.com>
2025-03-06 20:28:08 +0500

    Update script.js
    JS code for navigation bar properties, and also for alert message when adding information in Contact Me form.

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("Portfolio loaded!");
+document.querySelectorAll(".nav-link").forEach(link => {
+    link.addEventListener("click", function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute("href")).scrollIntoView({
+            behavior: "smooth"
+        });
+
+        document.querySelectorAll(".nav-link").forEach(item => item.classList.remove("active"));
+        this.classList.add("active");
+    });
+});
+
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    document.getElementById("formAlert").innerHTML = `<div class="alert alert-success">Message Sent Successfully!</div>`;
+});
